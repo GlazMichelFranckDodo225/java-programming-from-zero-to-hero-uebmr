ae83c14b99093ab8122fd4b7e65df45f733a065f
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-09-28 12:06:46 +0200

    Method Signature

--- src/com/dgmf/basics/b/classes/ArithmeticOperations.java
+++ src/com/dgmf/basics/b/classes/ArithmeticOperations.java
@@ -6,4 +6,16 @@ public class ArithmeticOperations {
 
         return result;
     }
+
+    // public int sum(int num1, int num2) { // Same Method Signature
+    // public int sum(int num3, int num4) { // Same Method Signature
+    // int sum(int num3, int num4) { // Same Method Signature
+    // long sum(int num3, int num4) { // Same Method Signature
+    // public int sum1(int num3, int num4) { // Ok
+    public int sum(byte num3, int num4) { // Ok
+        // int result = num1 + num2; // Compile-time Error ==> Parameters don't match
+        int result = num3 + num4;
+
+        return result;
+    }
 }
